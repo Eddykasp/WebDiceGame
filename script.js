839f8e2690ec57612bb600d82c9794e3f69d9eb9
Eddykasp <maxkasperowski@gmail.com>
2015-06-26 23:00:13 +0800

    Add previous work
    add html skeleton
    add javscript function getPlayer()
    add css some style elements

---
+++ script.js
@@ -0,0 +1,11 @@
+function getPlayers() {
+    var input = Number(document.getElementById("playersInput").value),
+        str = "";
+    if (isNaN(input)) {
+        str = "Not a valid input.";
+    } else {
+        str = input + " players playing."
+    };
+    document.getElementById("playersLabel").innerHTML = str;
+    return input;
+}

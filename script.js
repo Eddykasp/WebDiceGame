66078bfca4658e1d341dd1ca1f6a7a46bb3bfc45
Eddykasp <maxkasperowski@gmail.com>
2015-06-26 23:14:16 +0800

    add playGame function in js
    change indent to two spaces

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function getPlayers() {
-    var input = Number(document.getElementById("playersInput").value),
-        str = "";
-    if (isNaN(input)) {
-        str = "Not a valid input.";
-    } else {
-        str = input + " players playing."
-    };
-    document.getElementById("playersLabel").innerHTML = str;
-    return input;
+  var input = Number(document.getElementById("playersInput").value),
+    str = "";
+  if (isNaN(input)) {
+    str = "Not a valid input.";
+  } else {
+    str = input + " players playing."
+  };
+  document.getElementById("playersLabel").innerHTML = str;
+  return input;
+}
+
+function playGame() {
+  var players = getPlayers();
 }

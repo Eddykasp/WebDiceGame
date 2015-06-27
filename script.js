fb9a52984e2bc212e15f67ae7ffae94bc59e97b2
Eddykasp <maxkasperowski@gmail.com>
2015-06-27 13:20:56 +0800

    add game evaluation
    evaluate game and list winners
    rearrange table for displaying results

--- script.js
+++ script.js
@@ -10,13 +10,34 @@ function getPlayers() {
   return input;
 }
 
+function evaluateGame(scores) {
+  var max = 0, i = 0,
+  winners = [],
+  evalLabel = document.getElementById("evaluation");
+  for (i = 0; i < scores.length; i++) {
+    if (scores[i] > max) {
+      winners = [];
+      max = scores[i];
+      winners.push(i);
+    } else if (scores[i] == max) {
+      winners.push(i);
+    }
+  }
+  evalLabel.innerHTML = "";
+  for (i = 0; i < winners.length; i++) {
+    evalLabel.innerHTML += "Player " + (Number(winners[i]) + 1) +"<br>";
+  }
+}
+
 function playGame() {
   var players = getPlayers(),
   scores = [],
   i = 0,
   results = document.getElementById("results");
+  results.innerHTML = ""
   for (i = 0; i < players; i++) {
     scores[i] = Math.floor(Math.random() * 6)+1;
     results.innerHTML += "Player " + (i + 1) + ": " + scores[i] + "<br/>";
   }
+  evaluateGame(scores);
 }
